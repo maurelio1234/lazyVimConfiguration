719f4556ff87a8087e8c40a572da1d9a5f582c1a
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-24 14:20:20 +0100

    feat: tab=4 chars

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 
 vim.o.omnifunc = "syntaxcomplete#Complete"
+vim.opt.tabstop = 4 -- Set the width of a tab character
+vim.opt.shiftwidth = 4 -- Set the width of indentation levels
+vim.opt.expandtab = true -- Use spaces instead of tabs for indentation
