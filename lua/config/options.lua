5efc6abdacb628cbce5b8b3b669caf1e1440b3ca
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-24 19:01:11 +0100

    fix: turn off auto format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@ vim.o.omnifunc = "syntaxcomplete#Complete"
 vim.opt.tabstop = 4 -- Set the width of a tab character
 vim.opt.shiftwidth = 4 -- Set the width of indentation levels
 vim.opt.expandtab = true -- Use spaces instead of tabs for indentation
+vim.g.autoformat = false
