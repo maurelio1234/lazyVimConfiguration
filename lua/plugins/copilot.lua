a5074f49722da7d9691328f2304f05e8f8b3be94
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-24 12:20:19 +0100

    feat: configure copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        enabled = true,
+      },
+      filetypes = {
+        ["*"] = true,
+      },
+    },
+  },
+}
