95814d1416480cea5c2903653693f41b54b6a709
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-26 13:31:03 +0100

    feat: gitlinker

---
+++ lua/plugins/git.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        'ruifm/gitlinker.nvim',
+        depend  = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('gitlinker').setup()
+        end
+    }
+}
