6c907455d091ca4d9ad480ae498a41807926c660
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-26 19:03:13 +0100

    feat: introduce SwitchTestCode

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -18,3 +18,70 @@ vim.api.nvim_create_user_command("ToggleDarkLight", function(opts)
         vim.o.background = "dark"
     end
 end, {})
+
+
+vim.api.nvim_create_user_command("SwitchTestCode", function()
+    local function splitPath(path)
+        local parts = {}
+        for part in string.gmatch(path, "([^%.]+)") do
+            table.insert(parts, part)
+        end
+        return parts
+    end
+
+    local function isTestType(segment)
+        return segment == "spec" or segment == "test" or segment == "e2e" or segment == "unit"
+    end
+
+    local function fileExists(path)
+        return vim.fn.filereadable(path) == 1
+    end
+
+    local function findTestFile(baseName, extension)
+        local testTypes = {"unit", "spec", "test", "e2e"}
+        local dir = vim.fn.expand('%:p:h') .. '/'
+        for _, testType in ipairs(testTypes) do
+            local potentialTarget = dir .. baseName .. "." .. testType .. "." .. extension
+            if  fileExists(potentialTarget) then
+                return potentialTarget
+            end
+        end
+        return nil
+    end
+
+    local function tryToSwitchToSourceFile(parts, extension)
+        baseName = table.concat(parts, ".", 1, #parts - 2) -- Remove the test type
+        local potentialSource = vim.fn.expand('%:p:h') .. '/' .. baseName .. "." .. extension
+        if vim.fn.filereadable(potentialSource) == 1 then
+            vim.cmd('e ' .. potentialSource)
+            return
+        end
+    end
+
+    local function tryToSwitchToTestFile(baseName, extension)
+        local testFile = findTestFile(baseName, extension)
+        if testFile then
+            vim.cmd('e ' .. testFile)
+            return
+        end
+    end
+
+    -- Starts here
+    local filename = vim.fn.expand('%:t')
+    local parts = splitPath(filename)
+
+    if #parts < 2 then
+        print("File name format not recognized.")
+        return
+    end
+
+    local baseName, extension = table.concat(parts, ".", 1, #parts - 1), parts[#parts]
+    if isTestType(parts[#parts - 1]) then
+        tryToSwitchToSourceFile(parts, extension)
+    else
+        tryToSwitchToTestFile(baseName, extension)
+    end
+
+    print("Matching file not found.")
+end, {})
+
