d25b99b97a1be95dea19a00c4ac81f1401b043ba
Marcos Almeida <marcos.almeida@360learning.com>
2024-03-26 18:06:58 +0100

    feat: auto lcd to current directory when opening a file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- setting the working directory to the current buffer's directory
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    command = "lcd %:p:h",
+})
+
