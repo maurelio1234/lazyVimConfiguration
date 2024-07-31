c18c6aaaca406d9dd9fb91fec2a222ae9d24f748
Marcos Almeida <marcos.almeida@360learning.com>
2024-07-31 20:35:06 +0200

    feat: enable mouse in normal mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 
 vim.o.omnifunc = "syntaxcomplete#Complete"
+vim.o.mouse = "n"
 vim.opt.tabstop = 4 -- Set the width of a tab character
 vim.opt.shiftwidth = 4 -- Set the width of indentation levels
 vim.opt.expandtab = true -- Use spaces instead of tabs for indentation
