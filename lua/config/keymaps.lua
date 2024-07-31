8023ad953efc6aeb4a2de3c25f5576430e503a06
Marcos Almeida <marcos.almeida@360learning.com>
2024-07-31 21:16:50 +0200

    feat: cmd+c,v in neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,12 @@ vim.keymap.set("n", "<leader>gm", "<cmd>Magit<cr>", { desc = "Magit" })
 vim.keymap.set("n", "<leader>fN", "<cmd>ShowFilePathFromGitRoot<cr>", { desc = "Show current file path from git root" })
 vim.keymap.set("n", "<leader>cg", "<cmd>LiveGrepCurrentWord<cr>", { desc = "Grep current word in project "})
 vim.keymap.set("n", "<leader>cL", "<cmd>GitFilesCurrentWord<cr>", { desc = "Find file named after current word in project "})
+
+if vim.g.neovide then
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
