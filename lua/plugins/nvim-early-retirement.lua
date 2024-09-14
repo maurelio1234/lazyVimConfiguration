4dd13d4ed1afdff2a259a2cc5c17427938252c27
Marcos Almeida <marcos.almeida@360learning.com>
2024-09-14 12:25:10 +0200

    feat: introduce nvim-early-retirement

---
+++ lua/plugins/nvim-early-retirement.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "chrisgrieser/nvim-early-retirement",
+    config = true,
+    event = "VeryLazy",
+    opts = {
+      minimumBufferNum = 5,
+      notificationOnAutoClose = true
+    }
+  }
+}
