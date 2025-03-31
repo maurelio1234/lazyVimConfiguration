1dcb1bf6b97184dc8e5a0d9b7d80f802d202d9a2
Marcos Almeida <marcos.almeida@360learning.com>
2025-03-31 10:14:31 +0200

    feat: copilot chat add vue3 migration prompt

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,27 @@ return {
     },
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+      model = "claude-3.5-sonnet",
+      prompts = {
+        MigrateVue3 = {
+          prompt = "Migrate Vue 2 to Vue 3 composition API",
+          system_prompt = 'You are very good at migrating code stuff',
+          context = "buffer"
+        }
+      }
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    lazy = false -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/451
+  },
+  {
     "j-hui/fidget.nvim",
     tag = "v1.0.0",
     opts = {
