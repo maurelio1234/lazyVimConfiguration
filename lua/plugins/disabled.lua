7f20f8f71675dc5140b7906559865433e3e4c5f6
Marcos Almeida <marcos.almeida@360learning.com>
2025-03-31 10:14:49 +0200

    fix: disable hardtime
    conflicts with cmp

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,2 +1,9 @@
 return {
+  -- it conflicted with the new version of cmp
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+    enabled = false
+  }
 }
