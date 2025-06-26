c1a134a4d7b892485b1e79506348afdca2d85107
Marcos Almeida <marcos.almeida@360learning.com>
2025-06-26 10:12:46 +0200

    Revert "feat: lazygit better integration, & full screen"
    This reverts commit 101e49795e639987b12bd5574b57c03caf4f0f9f.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,20 +5,5 @@ return {
     config = function()
       require('gitlinker').setup()
     end
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitCurrentFile",
-      "LazyGitFilterCurrentFile",
-      "LazyGitFilter",
-    },
-    keys = {
-      { "<Leader>gg", "<cmd>LazyGit<CR>", desc = "lazygit" },
-    },
-    config = function()
-      vim.g.lazygit_floating_window_scaling_factor = 1
-    end,
   }
 }
