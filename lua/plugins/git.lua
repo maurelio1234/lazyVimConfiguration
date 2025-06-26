46d53e1e04589ddcc820bd6213d48a220bb2040a
Marcos Almeida <marcos.almeida@360learning.com>
2025-06-26 10:18:04 +0200

    feat: full screen via snack

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,5 +5,17 @@ return {
     config = function()
       require('gitlinker').setup()
     end
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      lazygit = {
+        -- https://github.com/folke/snacks.nvim/issues/719#issuecomment-2610398349
+        win = {
+          width = 0,
+          height = 0
+        },
+      }
+    }
   }
 }
